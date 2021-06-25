84198c609f3ebdff1e5fa14130232466a1a06281
ttdd2001@gmail.com <you@example.com>
2021-06-25 10:57:37 -0400

    Camera Stage

--- handtrackingenv/handTracking.py
+++ handtrackingenv/handTracking.py
@@ -5,8 +5,22 @@ import time
 # The number indicates which webcam you're using
 cap = cv2.VideoCapture(0)
 
+# Landmarking the hand
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+
 while True: 
     success, img = cap.read()
 
+    # Takes in the RGB image
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # Want to extract information from the results
+    results = hands.process(imgRGB)
+
+    #print(results)
+
+    # Runs the webcam
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
