480daf046a5050a70f9947155c1e0fd2f016c357
ttdd2001@gmail.com <you@example.com>
2021-06-28 11:33:43 -0400

    Final

--- handtrackingenv/handTracking.py
+++ handtrackingenv/handTracking.py
@@ -9,6 +9,11 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 
+mpDraw = mp.solutions.drawing_utils
+
+# Calculate the fps
+pTime = 0
+cTime = 0
 
 while True: 
     success, img = cap.read()
@@ -19,6 +24,29 @@ while True:
     results = hands.process(imgRGB)
 
     #print(results)
+    # Drawing the hand
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # 21 ids, numbered 0 to 20
+                # Each id contains three coordinate information
+                # Have to multiply each value by width and height to get pixels
+                # height, width, channel
+                h, w, c = img.shape
+                # Position of the center
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                # print(id, cx, cycC)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+    # Calculate fps
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    # Display fps on the screen
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 
+    3,(255,0,0),3)
 
     # Runs the webcam
     cv2.imshow("Image", img)
